a5c235c5ca7824b35c4aff96fddd8922d4e1f499
Zac M-W <z.millerwaugh@auckland.ac.nz>
2023-02-16 13:07:34 +1300

    Support creating pages, better detection of TinyMCE

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,17 @@ const state: stateObject = {
   configComponent: writable(null),
 };
 
+let attempts = 0;
 const getEditor = () =>
-  new Promise<Editor>((resolve) => {
+  new Promise<Editor>((resolve, reject) => {
+    if(!window.tinymce) {
+      setTimeout(() => {
+        // Try again after five seconds, waiting up to 30 seconds.
+        attempts++;
+        if (attempts < 6) resolve(getEditor());
+        else reject("Could not find TinyMCE editor");
+      }, 5000);
+    }
     if (!window.tinymce?.activeEditor) {
       // Try again once there is an active editor
       window.tinymce.on("AddEditor", ({ editor }: { editor: Editor }) => {
@@ -53,14 +62,19 @@ const loadToolbar = (props?: Toolbar["$$prop_def"]) => {
 
 export const loadApp = async () => {
   // Get TinyMCE Editor
-  const editor = await getEditor();
+  const editor = await getEditor().catch((e) => {
+    console.error(e);
+    return null;
+  });
+  if (!editor) return;
+  console.log("Editor loaded", editor);
 
   // Create Element Managers
   const grids = new GridManager(state, editor);
   const buttons = new ButtonManager(state, editor);
 
   // Add button to open grid editor
-  loadToolbar({ state, managers: [grids, buttons] });
+  const toolbar = loadToolbar({ state, managers: [grids, buttons] });
 
   // Inject our styles into the TinyMCE editor
   const editorStyles = document.createElement("style");
@@ -80,10 +94,20 @@ export const loadApp = async () => {
       document.body.classList.remove("cgb-toolbar-open");
     }
   });
+  // Hide app when TinyMCE editor is removed
+  editor.on("detach", () => {
+    console.log("Editor removed");
+    toolbar?.$destroy();
+    // Load the app again when the editor is reloaded
+    setTimeout(() => {
+      loadApp();
+    }, 100);
+  });
 };
 
+console.log("Loading app");
+window.addEventListener("load", loadApp);
 // Load the app only on certain pages
-const loc = window.location.pathname;
-if (/pages\/?$|pages\/.+\/edit$/.test(loc)) {
-  window.addEventListener("load", loadApp);
-}
+// const loc = window.location.pathname;
+// if (/pages\/?$|pages\/.+\/edit$/.test(loc)) {
+// }
