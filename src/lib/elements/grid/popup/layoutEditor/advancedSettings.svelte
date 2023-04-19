f653aa7545f66383a18a9c3a5f4cabbb1ae1149d
Zac M-W <z.millerwaugh@auckland.ac.nz>
2023-04-19 13:17:34 +1200

    Implement functionality for adjusting row margins manually

--- src/lib/elements/grid/popup/layoutEditor/advancedSettings.svelte
+++ src/lib/elements/grid/popup/layoutEditor/advancedSettings.svelte
@@ -19,15 +19,49 @@
   $: style = row.style;
   $: classList = row.classList;
 
+  let manualAdjustMargin: boolean | undefined,
+    manualAdjustPadding: boolean | undefined;
+  $: manualAdjustMargin = undefined;
+  $: manualAdjustPadding = undefined;
+
   $: preferences = writableDerived(
     [style, classList],
     ([$style, $classList]) => {
+      // Margin
+      let margin: number | [number, number, number, number];
+      if ($style.margin && !manualAdjustMargin) {
+        margin = toPx($style.marginTop) || 0;
+      } else if (manualAdjustMargin) {
+        margin = [
+          $style.marginTop,
+          $style.marginRight,
+          $style.marginBottom,
+          $style.marginLeft,
+        ].map((m) => toPx(m) || 0) as [number, number, number, number];
+      } else {
+        margin = 0;
+      }
+      // Padding
+      let padding: number | [number, number, number, number];
+      if ($style.padding && !manualAdjustPadding) {
+        padding = toPx($style.paddingTop) || 0;
+      } else if (manualAdjustPadding) {
+        padding = [
+          $style.paddingTop,
+          $style.paddingRight,
+          $style.paddingBottom,
+          $style.paddingLeft,
+        ].map((m) => toPx(m) || 0) as [number, number, number, number];
+      } else {
+        padding = 0;
+      }
+      // Card
       const isCard =
         $classList.contains("uoa_shadowbox") &&
         $classList.contains("uoa_corners_4");
       return {
-        padding: $style.padding ? toPx($style.padding) : 0,
-        margin: $style.margin ? toPx($style.margin) : 0,
+        padding: padding,
+        margin: margin,
         background: getColour($style.background),
         textColor: getColour($style.color),
         card: isCard ? RowType.Card : RowType.Normal,
@@ -47,7 +81,17 @@
         }
         if (oldStyle) {
           // Padding
-          oldStyle.padding = `${reflecting.padding}px`;
+          if (typeof reflecting.padding === "number")
+            oldStyle.padding = `${reflecting.padding}px`;
+          else if (Array.isArray(reflecting.padding))
+            oldStyle.padding = reflecting.padding
+              .map((p) => `${p}px`)
+              .join(" ");
+          // Margin
+          if (typeof reflecting.margin === "number")
+            oldStyle.margin = `${reflecting.margin}px`;
+          else if (Array.isArray(reflecting.margin))
+            oldStyle.margin = reflecting.margin.map((m) => `${m}px`).join(" ");
           // Background
           oldStyle.background = reflecting.background?.toHex() || "";
           // Text Colour
@@ -60,6 +104,7 @@
 
   const ids = {
     padding: nanoid(),
+    margin: nanoid(),
     card: nanoid(),
     background: nanoid(),
     textcolor: nanoid(),
@@ -90,18 +135,150 @@
         />
       </label>
     </div>
-    <label for={ids.padding}>
-      <span class="label-text"
-        >Padding ({Math.round($preferences.padding || 0)}px)</span
-      >
+    <!-- Checkbox for individually adjusting padding -->
+    <label class="checkbox">
       <input
-        id={ids.padding}
-        type="range"
-        min="0"
-        max="20"
-        bind:value={$preferences.padding}
+        type="checkbox"
+        bind:checked={manualAdjustPadding}
+        class="checkbox-input"
       />
+      <span class="checkbox-label">Manual Adjust Padding</span>
     </label>
+    {#if !manualAdjustPadding && typeof $preferences.padding === "number"}
+      <label for={ids.padding}>
+        <span class="label-text"
+          >Padding ({Math.round($preferences.padding || 0)}px)</span
+        >
+        <input
+          id={ids.padding}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.padding}
+        />
+      </label>
+    {:else if typeof $preferences.padding !== "number"}
+      <label for={ids.padding + "-top"}>
+        <span class="label-text"
+          >Padding Top ({Math.round($preferences.padding[0] || 0)}px)</span
+        >
+        <input
+          id={ids.padding + "-top"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.padding[0]}
+        />
+      </label>
+      <label for={ids.padding + "-right"}>
+        <span class="label-text"
+          >Padding Right ({Math.round($preferences.padding[1] || 0)}px)</span
+        >
+        <input
+          id={ids.padding + "-right"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.padding[1]}
+        />
+      </label>
+      <label for={ids.padding + "-bottom"}>
+        <span class="label-text"
+          >Padding Bottom ({Math.round($preferences.padding[2] || 0)}px)</span
+        >
+        <input
+          id={ids.padding + "-bottom"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.padding[2]}
+        />
+      </label>
+      <label for={ids.padding + "-left"}>
+        <span class="label-text"
+          >Padding Left ({Math.round($preferences.padding[3] || 0)}px)</span
+        >
+        <input
+          id={ids.padding + "-left"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.padding[3]}
+        />
+      </label>
+    {/if}
+    <!-- Checkbox for individually adjusting margin -->
+    <label class="checkbox">
+      <input
+        type="checkbox"
+        bind:checked={manualAdjustMargin}
+        class="checkbox-input"
+      />
+      <span class="checkbox-label">Manual Adjust Margin</span>
+    </label>
+    {#if !manualAdjustMargin}
+      <label for={ids.margin}>
+        <span class="label-text"
+          >Margin ({Math.round($preferences.margin || 0)}px)</span
+        >
+        <input
+          id={ids.margin}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.margin}
+        />
+      </label>
+    {:else if typeof $preferences.margin !== "number"}
+      <label for={ids.margin + "-top"}>
+        <span class="label-text"
+          >Margin Top ({Math.round($preferences.margin[0] || 0)}px)</span
+        >
+        <input
+          id={ids.margin + "-top"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.margin[0]}
+        />
+      </label>
+      <label for={ids.margin + "-right"}>
+        <span class="label-text"
+          >Margin Right ({Math.round($preferences.margin[1] || 0)}px)</span
+        >
+        <input
+          id={ids.margin + "-right"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.margin[1]}
+        />
+      </label>
+      <label for={ids.margin + "-bottom"}>
+        <span class="label-text"
+          >Margin Bottom ({Math.round($preferences.margin[2] || 0)}px)</span
+        >
+        <input
+          id={ids.margin + "-bottom"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.margin[2]}
+        />
+      </label>
+      <label for={ids.margin + "-left"}>
+        <span class="label-text"
+          >Margin Left ({Math.round($preferences.margin[3] || 0)}px)</span
+        >
+        <input
+          id={ids.margin + "-left"}
+          type="range"
+          min="0"
+          max="20"
+          bind:value={$preferences.margin[3]}
+        />
+      </label>
+    {/if}
     <ColourSettings element={row} {preferences} />
   </div>
 </div>
