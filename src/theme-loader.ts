d0e19db636bfcc2c943f5aaf1d094564884d2880
Zac M-W <z.millerwaugh@auckland.ac.nz>
2023-08-10 18:08:45 +1200

    Attempt to enable CORS

--- src/theme-loader.ts
+++ src/theme-loader.ts
@@ -4,7 +4,7 @@
 const script = document.createElement("script");
 script.src = `${
   import.meta.env.CANVAS_BLOCKS_THEME_HOST
-}theme.js?${Date.now()}`;
+}canvas-blocks.min.js?v=${__APP_VERSION__}`;
 script.type = "module";
 (document.head || document.documentElement).appendChild(script);
 /**
