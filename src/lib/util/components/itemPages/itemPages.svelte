0e20ad1370bf275883d88b3adaec71123c4df458
Zac M-W <z.millerwaugh@auckland.ac.nz>
2023-10-24 12:15:56 +1300

    Make generic multi-item element component

---
+++ src/lib/util/components/itemPages/itemPages.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { ComponentProps, createEventDispatcher } from "svelte";
+  import OrderableList from "../orderableList.svelte";
+  import { nanoid } from "nanoid";
+
+  export let items: Record<string, any>[] = [];
+  export let idKey = "id";
+  export let addDefaults: false | Record<any, any> = false;
+  export let OrderableListOptions: Partial<
+    Pick<
+      ComponentProps<OrderableList>,
+      "labelKey" | "idKey" | "showEdit" | "canDelete"
+    >
+  >;
+
+  export let selectedId: any | null = null;
+  $: selectedIndex = items.findIndex((item) => item[idKey] === selectedId);
+  const dispatch = createEventDispatcher<{
+    edit: string;
+    delete: string;
+  }>();
+
+  $: canDelete = OrderableListOptions.canDelete !== false && items.length > 1;
+
+  const addItem = () => {
+    items.push({
+      ...addDefaults,
+      [idKey]: nanoid(),
+    });
+    selectedId = items[items.length - 1].id;
+    items = items;
+  };
+</script>
+
+<div class="itemGrid">
+  <div class="col">
+    <OrderableList
+      labelKey="label"
+      {idKey}
+      showEdit={true}
+      {...OrderableListOptions}
+      bind:items
+      on:edit={({ detail: id }) => {
+        dispatch("edit", id);
+        selectedId = id;
+      }}
+      on:delete={({ detail: id }) => {
+        dispatch("delete", id);
+        const index = items.findIndex((i) => i[idKey] === id);
+        if (selectedId === id) {
+          if (index > 0) selectedId = items[index - 1].moduleID;
+          else selectedId = items[0].moduleID;
+        }
+      }}
+      {canDelete}
+    />
+    {#if addDefaults}
+      <button class="Button Button--small" on:click={() => addItem()}>
+        <i class="icon-plus" aria-hidden="true" />
+        Add Card</button
+      >
+    {/if}
+  </div>
+  <div class="col">
+    <slot name="header" />
+    {#if selectedIndex !== undefined}
+      <slot itemIndex={selectedIndex} />
+    {/if}
+  </div>
+</div>
+
+<style lang="postcss">
+  .itemGrid {
+    @apply grid grid-flow-col grid-cols-2 gap-4;
+  }
+  .col {
+    @apply flex flex-col gap-2;
+  }
+</style>
