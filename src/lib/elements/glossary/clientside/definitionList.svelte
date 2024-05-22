4da7683b404f7ccc6420b06809490cf57e2036e5
Zac M-W <z.millerwaugh@auckland.ac.nz>
2024-05-22 15:16:46 +1200

    Revamp glossary look on mobile app to fit native theme better

--- src/lib/elements/glossary/clientside/definitionList.svelte
+++ src/lib/elements/glossary/clientside/definitionList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { courseEnv } from "$lib/util/courseEnv";
   import { nanoid } from "nanoid";
   import { slide } from "svelte/transition";
 
@@ -11,14 +12,19 @@
   );
   let id = nanoid();
   let expanded = false;
+  let isMobile = import.meta.env.MODE.includes("mobile");
 </script>
 
 {#if foundTerms.length > 0}
   <div class="cgb-component">
-    <div class="definitionList" class:defList--expanded={expanded}>
-      <h2 class="leading-none text-xl">
+    <div
+      class="definitionList"
+      class:defList--mobile={isMobile}
+      class:defList--expanded={expanded}
+    >
+      <h2 class="leading-none m-0 text-xl">
         <button
-          class="text-lg text-left w-full transition focus:outline-none focus-visible:ring-2 ring-primary rounded px-3 py-2"
+          class="defList--button"
           {id}
           aria-expanded={expanded}
           aria-controls="{id}-details"
@@ -27,7 +33,7 @@
           }}
         >
           <span class="icon pr-2">
-            <span class="cdb--icon transition" class:rotate-180={expanded}>
+            <span class="cdb--icon transition" class:expanded>
               Canvas.arrow-open-up
             </span>
           </span>
@@ -76,8 +82,39 @@
         @apply border-b border-gray-100;
       }
     }
+    & .defList--button {
+      @apply text-lg text-left w-full transition ring-primary rounded px-3 py-2;
+      @apply focus:outline-none focus-visible:ring-2;
+      & .icon {
+        & .expanded {
+          @apply -scale-y-100;
+        }
+      }
+    }
     & .defList--details {
       @apply overflow-hidden px-3;
+    }
+    dl {
+      @apply mb-0 pb-3;
+    }
+
+    &.defList--mobile {
+      @apply fixed bottom-0 inset-x-0 w-full z-50 bg-[#F4F4F4] border-none rounded-t-xl;
+      & .defList--button {
+        @apply py-3;
+        & .icon {
+          @apply float-right;
+        }
+      }
+      &.defList--expanded {
+        @apply mb-0 rounded-b-none border-none;
+      }
+      dt {
+        @apply text-lg font-bold;
+      }
+      dd {
+        @apply text-base mx-0;
+      }
     }
   }
 </style>
